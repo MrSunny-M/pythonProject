d3b2f0727354e217730f32b24e03432594802ea9
Sunny <Saikiran.M@outlook.in>
2022-05-04 23:52:53 +0530

    Added new lines

--- OSreboot.py
+++ OSreboot.py
@@ -1,10 +1,15 @@
-import OS
+import os
+import sys
 
 def check_reboot:
     """Returns True if server has a pending restart"""
-    return os.path.exists("/run/reboot-required")
+    return os.path.exist("/run/reboot-required")
 
 def main():
-    pass
+    if check_reboot():
+        print("pending restart")
+        sys.exit(1)
+    print("Everything is fine")
+    sys.exit(0)
 
 main()
